fbb904df442b44ff7f44c90c941696ee1bd918bf
Fran Petković <fran.petkovic1@gmail.com>
2022-04-22 18:51:25 +0200

    Dark and light mode

---
+++ script.js
@@ -0,0 +1,7 @@
+const chk = document.getElementById('chk');
+const lopta = document.getElementById(".ball");
+const fran1 = document.querySelector(".fran1");
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
